8f7e305d80ddf5a0b53245f8ec7995294d5cb70f
Darkin45 <117683619+Darkin45@users.noreply.github.com>
2023-05-13 09:35:04 -0300

    Add files via upload

---
+++ tp/login.js
@@ -0,0 +1,19 @@
+function loguear()
+{
+
+var user= document.getElementById("usuario").value;
+
+var pass= document.getElementById("pass").value;
+
+if(user=="Elias" && pass=="45034700")
+
+{
+
+
+window.location="index.html";
+
+}
+else {alert("incorrecto")}
+
+
+}
